9d541c5722842df2af3157f098c7e09d0bd8906e
Ketania Govender <82605074+ketaniaaa@users.noreply.github.com>
2022-11-14 15:36:09 +0200

    init

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
+
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+
+const PlaceholderImage = require('./assets/asset4.png');
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+   
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.imageContainer}>
+    <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />    
+    <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
     </View>
+    <View style={styles.footerContainer}>
+    <Button theme="primary" label="Choose a photo" />
+
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+    <StatusBar style="auto" />
+  </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,    
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
